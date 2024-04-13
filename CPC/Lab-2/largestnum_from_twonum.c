bdc7385fcc22141f3a98abe82ff2e39efc683c30
shrutizalariya <146011537+shrutizalariya@users.noreply.github.com>
2024-04-13 18:11:38 +0530

    Add files via upload

---
+++ CPC/Lab-2/largestnum_from_twonum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+  void main(){
+  	int a,b;
+  	printf("Enter A:");
+  	scanf("%d",&a);
+  	printf("Enter B:");
+  	scanf("%d",&b);
+  	if (a>b)
+  	{
+  		printf("A is greater");
+	}
+	else
+	{
+		printf("B is greater");
+	}
+}
