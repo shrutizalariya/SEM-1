b1079cb569d7209a50db91a37fb3d8ed9e8efaed
shrutizalariya <146011537+shrutizalariya@users.noreply.github.com>
2024-04-13 18:17:53 +0530

    Add files via upload

---
+++ CPC/Lab-5/leftandrightsift.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+   void main(){
+   	int a,b,c;
+   	printf("Enter a:");
+   	scanf("%d",&a);
+   	b=a<<1;
+   	c=a>>1;
+   	printf("sum is = %d %d",b,c);
+}
---
+++ CPC/Lab-5/num_is_even_or_odd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+   void main(){
+   	    int a,c;
+   	    printf("Enter num a:");
+   	    scanf("%d",&a);
+   	    c=a&1;
+   	  if (c==0)
+   	  {
+   	  	printf("num a is even");
+	  }
+	  else
+	  {
+	  	printf("num a is odd");
+	  }
+   }
