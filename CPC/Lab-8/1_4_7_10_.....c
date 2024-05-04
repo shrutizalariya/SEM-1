fcda695f14eec66b969424da258aea2b286da257
shrutizalariya <146011537+shrutizalariya@users.noreply.github.com>
2024-05-04 15:17:52 +0530

    Add files via upload

---
+++ CPC/Lab-8/1_4_7_10_.....c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+    void main(){
+    	int i=1,n=1;
+    	while(i<51){
+    		n=n+3;
+    		printf("%d\n",n);
+    		i++;
+		}
+}
---
+++ CPC/Lab-8/sqrt_of_0_to_n_num.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+    void main(){
+    	int i=0;
+    	float n;
+    	while(i<=9){
+    		printf("%d\n",i);
+    		n=sqrt(i);
+    		printf("%f\n",n);
+    		i++;
+		}
+		//printf("%d",n);
+	}
+	
