cf2a3cc01a22c0736a10b3631a59ef78ac0abaf4
shrutizalariya <146011537+shrutizalariya@users.noreply.github.com>
2024-06-03 22:00:45 +0530

    Add files via upload

---
+++ CPC/Lab-16/transpose_of_a_matrix.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+    void main(){
+    	int i,j,n,m;
+    	printf("Enter n:");
+    	scanf("%d",&n);
+    	printf("Enter m:");
+    	scanf("%d",&m);
+    	int a[n][m];
+    	for(i=0;i<n;i++){
+    		for(j=0;j<m;j++){
+    			printf("a[%d][%d]=",i,j);
+    			scanf("%d",&a[i][j]);
+			}
+		}
+		for(i=0;i<n;i++){
+    		for(j=0;j<m;j++){
+    			printf("%d ",a[j][i]);
+    		}
+    			printf("\n");
+    	}
+    }
